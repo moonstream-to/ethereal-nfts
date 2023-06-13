60c63bc1991d5047dfbb696439b3609bbb20db3f
Neeraj Kashyap <neeraj@bugout.dev>
2023-06-13 15:55:52 -0700

    Started working on relayers

---
+++ relayers/main.go
@@ -0,0 +1,15 @@
+// Relayers API server and command-line interface.
+//
+// The github.com/moonstream-to/ethereal-nfts/relayers package is the entrypoint to the libre (free and open source)
+// Ethereal NFT relayers tooling. This package defines the structure of the relayers API and also defines
+// the command-line interface that can be used to configure and start the API server.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello world")
+}
